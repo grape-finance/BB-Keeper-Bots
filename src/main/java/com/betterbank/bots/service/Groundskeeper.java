bc9871b27cf89c4551d21470e9ecdeea7aa549d3
Konstantin Pribluda <konstantin.pribluda@gmail.com>
2025-06-28 15:02:06 +0300

    both services scheduled and are working and sending TX

--- src/main/java/com/betterbank/bots/service/Groundskeeper.java
+++ src/main/java/com/betterbank/bots/service/Groundskeeper.java
@@ -1,9 +1,23 @@
 package com.betterbank.bots.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.web3j.protocol.Web3jService;
+import org.web3j.crypto.Credentials;
+import org.web3j.generated.contracts.IMintRedeemer;
+import org.web3j.generated.contracts.ITreasury;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.DefaultBlockParameterName;
+import org.web3j.protocol.core.methods.response.EthBlock;
+import org.web3j.protocol.core.methods.response.EthSendTransaction;
+import org.web3j.tx.RawTransactionManager;
+import org.web3j.tx.TransactionManager;
+import org.web3j.tx.gas.DynamicGasProvider;
 
+import java.io.IOException;
+import java.math.BigInteger;
 import java.util.List;
 
 /**
@@ -13,15 +27,94 @@ import java.util.List;
 @Service
 public class Groundskeeper {
 
-    private final Web3jService web3jService;
-
+    private final TransactionManager txManager;
+    private final Web3j web3j;
+    private final Credentials credentials;
+    private final long chainId;
     private final String mintRedeemer;
-    private final List<String>  treasuries;
-    public Groundskeeper(Web3jService web3jService,
+    private final List<String> treasuries;
+    private final BigInteger gasLimit;
+    Logger logger = LoggerFactory.getLogger(Groundskeeper.class);
+
+
+    private final DynamicGasProvider dynamicGasProvider;
+
+    private final List<ITreasury> iTreasuries;
+    private final IMintRedeemer iMintRedeemer;
+
+    public Groundskeeper(Web3j web3j,
                          @Value("${groundskeeper.mintRedeemer}") String mintRedeemer,
-                         @Value("${groundskeeper.treasuries}") List<String> treasuries) {
-        this.web3jService = web3jService;
+                         @Value("${groundskeeper.treasuries}") List<String> treasuries,
+                         @Value("${groundskeeper.executor}") String executor,
+                         @Value("${web3j.node.chainId}") long chainId,
+                         @Value("${groundskeeper.gasLimit}") int gasLimit) {
+
+        this.credentials = Credentials.create(executor);
+        this.dynamicGasProvider = new DynamicGasProvider(web3j);
+        this.chainId = chainId;
+        this.txManager = new RawTransactionManager(web3j, credentials, this.chainId);
+
         this.mintRedeemer = mintRedeemer;
+        iMintRedeemer = IMintRedeemer.load(this.mintRedeemer, web3j, credentials, dynamicGasProvider);
         this.treasuries = treasuries;
+        iTreasuries = this.treasuries.stream().map(t -> ITreasury.load(t, web3j, credentials, dynamicGasProvider)).toList();
+        this.web3j = web3j;
+        this.gasLimit = BigInteger.valueOf(gasLimit);
+    }
+
+
+    @Scheduled(cron = "${groundskeeper.updateEsteemRate.cron}")
+    public void updateEsteemRate() {
+        logger.info("updating esteem rate");
+
+        try {
+            EthBlock.Block block = web3j.ethGetBlockByNumber(DefaultBlockParameterName.LATEST, false).send().getBlock();
+            BigInteger gasPrice = block.getBaseFeePerGas().multiply(BigInteger.TEN).divide(BigInteger.valueOf(100));
+
+            String encodedFunctionCall = iMintRedeemer.updateEsteemRate().encodeFunctionCall();
+
+
+            EthSendTransaction sent = txManager.sendEIP1559Transaction(
+                    chainId,
+                    gasPrice,
+                    gasPrice,
+                    gasLimit,
+                    mintRedeemer,
+                    encodedFunctionCall,
+                    null
+            );
+
+            logger.info("update esteem rate tx: {}", sent.getTransactionHash());
+        } catch (Exception e) {
+            logger.error("error while updating esteem rate", e);
+        }
+    }
+
+    @Scheduled(cron = "${groundskeeper.allocateSeignorage.cron}")
+    public void allocateSeignorage() throws IOException {
+
+        EthBlock.Block block = web3j.ethGetBlockByNumber(DefaultBlockParameterName.LATEST, false).send().getBlock();
+        BigInteger gasPrice = block.getBaseFeePerGas().multiply(BigInteger.TEN).divide(BigInteger.valueOf(100));
+
+        iTreasuries.forEach(t -> {
+            try {
+
+                logger.info("allocating seignorage to {}", t.getContractAddress());
+
+                String encodedFunctionCall = t.allocateSeigniorage().encodeFunctionCall();
+                EthSendTransaction sent = txManager.sendEIP1559Transaction(
+                        chainId,
+                        gasPrice,
+                        gasPrice,
+                        gasLimit,
+                        t.getContractAddress(),
+                        encodedFunctionCall,
+                        null
+                );
+                logger.info("tx sent {}", sent.getTransactionHash());
+            } catch (Exception e) {
+                logger.error("error while allocating seignorage", e);
+            }
+        });
     }
 }
